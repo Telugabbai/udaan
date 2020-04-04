a21866983c12136a5a9eeee7511029909b885b08
Telugabbai <mekalasreenivasarao8@gmail.com>
2020-04-05 00:24:23 +0530

    Modified employee/models.py:

--- employee/models.py
+++ employee/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    emp_name = models.CharField(max_length=30)
+    emp_name = models.CharField(max_length=30,min_length=4)
     phone = models.CharField(max_length=15)
     salary = models.IntegerField()
     city = models.CharField(max_length=20)
